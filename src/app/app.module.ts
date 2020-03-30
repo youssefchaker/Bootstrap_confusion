4526265b38f43f31ede786d81628908b8a49774a
youssef chaker <youssef.chaker@etudiant-isi.utm.tn>
2020-03-30 17:01:48 +0100

    basic service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
-
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import 'hammerjs';
+import {MatListModule} from '@angular/material/list';
+import { MenuComponent } from './menu/menu.component';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import {DishService} from './services/dish.service';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    DishdetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    FlexLayoutModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule
   ],
-  providers: [],
+  providers: [
+    DishService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/shared/dish.ts
@@ -0,0 +1,12 @@
+import {Comment} from './Comment';
+export class Dish{
+    id:string;
+    name: string;
+    image: string;
+    category:string;
+    featured:boolean;
+    label:string;
+    price:string;
+    description:string;
+    comments: Comment[];
+}
