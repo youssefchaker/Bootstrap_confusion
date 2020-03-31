78d3ab87b09ddc62c50f6bcbdf3e782d3023d82a
youssef chaker <youssef.chaker@etudiant-isi.utm.tn>
2020-03-31 20:41:17 +0100

    Single Page Applications Part 1

---
+++ src/app/app-routing/routes.ts
@@ -0,0 +1,13 @@
+import {Routes} from '@angular/router';
+import { HomeComponent } from '../home/home.component';
+import { AboutComponent } from '../about/about.component';
+import { ContactComponent } from '../contact/contact.component';
+import { DishdetailComponent } from '../dishdetail/dishdetail.component';
+import { MenuComponent } from '../menu/menu.component';
+
+export const routes: Routes=[
+    {path: 'home' , component: HomeComponent},
+    {path: 'menu' , component: MenuComponent},
+    {path: 'contactus' , component: ContactComponent},
+    {path: '' , redirectTo: '/home' , pathMatch: 'full'}
+];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,11 +12,23 @@ import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 import {DishService} from './services/dish.service';
+import {PromotionService} from './services/promotion.service';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import {AppRoutingModule} from './app-routing/app-routing.module';
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishdetailComponent
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
@@ -26,10 +38,12 @@ import {DishService} from './services/dish.service';
     MatListModule,
     MatGridListModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    AppRoutingModule
   ],
   providers: [
-    DishService
+    DishService,
+    PromotionService
   ],
   bootstrap: [AppComponent]
 })
