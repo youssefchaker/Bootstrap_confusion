a80b8051bbbd0d63b7b513b4e6ff0dc1d89e2edb
youssef chaker <youssef.chaker@etudiant-isi.utm.tn>
2020-04-03 18:01:02 +0100

    promise part 1

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -18,9 +18,12 @@ export class HomeComponent implements OnInit {
   constructor(private dishService: DishService, private promotionService: PromotionService , private leaderservice: LeaderService) { }
 
   ngOnInit() {
-    this.dish = this.dishService.getFeaturedDish();
-    this.promotion = this.promotionService.getFeaturedPromotion();
-    this.leader = this.leaderservice.getFeaturedDish();
+     this.dishService.getFeaturedDish()
+    .then((dish) =>this.dish =dish)
+     this.promotionService.getFeaturedPromotion()
+    .then((promotion) => this.promotion=promotion);
+    this.leaderservice.getFeaturedDish()
+    .then((leader) => this.leader =leader );
   }
 
 }
